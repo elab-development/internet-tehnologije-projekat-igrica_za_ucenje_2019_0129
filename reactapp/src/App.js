62b07f34e8c20fd7aef34df7b59928a011efd10d
OgnjenJankovic <oj20190129@student.fon.bg.ac.rs>
2024-09-06 00:23:54 +0200

    register form komponenta

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginForm from './LoginForm';  
 import FlexboxGame from './FlexboxGame';  
 import Pocetna from './Pocetna';
+import RegisterForm from './RegisterForm';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Pocetna />} />
         <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+
         <Route path="/flexboxfroggygame" element={<FlexboxGame />} />
       </Routes>
     </Router>
---
+++ reactapp/src/RegisterForm.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import './LoginForm.css';   
+
+const RegisterForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/api/register', {
+        name,
+        email,
+        password,
+        password_confirmation: passwordConfirmation,
+      });
+
+     
+      navigate('/login');
+    } catch (err) {
+      setError('Registration failed. Please try again.');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Registruj se</h2>
+      {error && <p className="error-message">{error}</p>}
+      <form onSubmit={handleRegister}>
+        <div className="form-group">
+          <label htmlFor="name">Ime:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password">Lozinka:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password_confirmation">Potvrdi lozinku:</label>
+          <input
+            type="password"
+            id="password_confirmation"
+            value={passwordConfirmation}
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
+            required
+          />
+        </div>
+
+        <button type="submit" className="login-button">Registruj se</button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterForm;
